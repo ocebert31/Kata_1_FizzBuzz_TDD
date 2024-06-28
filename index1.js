a8ef1b2fe083e1c47380af280e2c43a821e0b667
ocebert31 <153503666+ocebert31@users.noreply.github.com>
2024-06-28 17:38:51 +0200

    ajout du test Buzz

--- index1.js
+++ index1.js
@@ -16,6 +16,8 @@ function fizzBuzz() {
     for (let i = 0; i < 100; i ++) {
         if (i % 3 === 0) {
             numbers.push('Fizz')
+        } else if (i === 5) {
+            numbers.push('Buzz');
         } else {
             numbers.push(i);
         }
@@ -33,4 +35,8 @@ assert('3 become Fizz', () => {
 
 assert('6 become Fizz', () => {
     return fizzBuzz()[6] === 'Fizz';
+})
+
+assert('5 become Buzz', () => {
+    return fizzBuzz()[5] === 'Buzz';
 })
